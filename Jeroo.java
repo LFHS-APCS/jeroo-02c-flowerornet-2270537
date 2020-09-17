80acf986c8b80626ad39a6da8106fa66cad4e348
2270537 <69986575+2270537@users.noreply.github.com>
2020-09-17 15:14:30 -0500

    Update Jeroo.java

--- Jeroo.java
+++ Jeroo.java
@@ -9,36 +9,38 @@ public class Jeroo extends JerooBase {
      *     in the front of the Jeroo?
      *  No side effects and no errors.
      */
-   public boolean isFlowerOrNetNearInFront() {
+  public boolean isFlowerOrNetNearInFront() {
       if (isClear(AHEAD)){
-      hop();
-      if (isFlower(AHEAD)){
-       turn(LEFT);
-      turn(LEFT);
-      hop();
-      turn(LEFT);
-      turn(LEFT); 
-      return true;
-      }
-      else if (isNet(AHEAD)){
-      turn(LEFT);
-      turn(LEFT);
-      hop();
-      turn(LEFT);
-      turn(LEFT); 
-      return true;
-      }
-      else {
+        hop();
+        if (isFlower(AHEAD)){
         turn(LEFT);
-      turn(LEFT);
-      hop();
-      turn(LEFT);
-      turn(LEFT);
+        turn(LEFT);
+        hop();
+        turn(LEFT);
+        turn(LEFT);
+        return true;
+        }
+        else if (isNet(AHEAD)){
+        turn(LEFT);
+        turn(LEFT);
+        hop();
+        turn(LEFT);
+        turn(LEFT);
+        return true;
+        }
+        else {
+        turn(LEFT);
+        turn(LEFT);
+        hop();
+        turn(LEFT);
+        turn(LEFT);
         return false;
-      }
-      }
-    return false;
-    }
+       }
+      
+     }
+     return false;
+   }
+
 
     // Do NOT touch the code below here
     public Jeroo() {super();}
